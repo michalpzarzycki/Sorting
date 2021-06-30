0e22c7db8eb55598343245f11099a46dd75b91b0
Michal <michal.p.zarzycki@wp.pl>
2021-06-30 18:51:02 +0200

    Got rid of unwanted div

--- src/components/SortingAlgorithm.js
+++ src/components/SortingAlgorithm.js
@@ -19,7 +19,6 @@ function SortingAlgorithm() {
             <Button handleClick={() => setRandom(!random)} placeholder="RANDOM AGAIN!" />
             <Button handleClick={() => setArray([...bubbleSort(array)])} placeholder="BUBBLE SORT" />
             <div className={styles.barContainer}>
-                <div>{bubbleSort([2, 5, 1, 1, 5, 6])}</div>
                 {/* display an array as a rectangles with height of arr[i] */}
                 {array.map((x, idx) => <div key={idx} className={styles.bar} style={{ height: `${x}px` }}></div>)}
             </div>
