0dc9d7bfbb589d05a9f7dde63ac7e4594f5d2d01
Michal <michal.p.zarzycki@wp.pl>
2021-06-30 18:50:11 +0200

    Added static bubble sort

--- src/components/SortingAlgorithm.js
+++ src/components/SortingAlgorithm.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { randomNumberGenerator } from '../utils/randomNumberGenerator'
 import { createAnArray } from '../utils/createAnArray'
+import { bubbleSort } from '../algorithms/bubbleSort'
 import styles from './SortingAlgorithm.module.css';
 import Button from './Button';
 
@@ -10,12 +11,15 @@ function SortingAlgorithm() {
 
     useEffect(() => {
         setArray(createAnArray(100, () => randomNumberGenerator(10, 600)))
+
     }, [random])
 
     return (
         <>
             <Button handleClick={() => setRandom(!random)} placeholder="RANDOM AGAIN!" />
+            <Button handleClick={() => setArray([...bubbleSort(array)])} placeholder="BUBBLE SORT" />
             <div className={styles.barContainer}>
+                <div>{bubbleSort([2, 5, 1, 1, 5, 6])}</div>
                 {/* display an array as a rectangles with height of arr[i] */}
                 {array.map((x, idx) => <div key={idx} className={styles.bar} style={{ height: `${x}px` }}></div>)}
             </div>
