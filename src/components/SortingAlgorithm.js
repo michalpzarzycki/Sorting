30d8f7d786890aee917bb630f2f14f7264479775
Michal <michal.p.zarzycki@wp.pl>
2021-06-30 17:48:42 +0200

    Created SortingAlgorithm with tofo

---
+++ src/components/SortingAlgorithm.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import styles from './SortingAlgorithm.module.css';
+function SortingAlgorithm() {
+    const [array, setArray] = useState([]);
+
+    useEffect(() => {
+        //create an array of random numbers
+    }, [])
+
+    return (
+        <div>
+            {/* display an array as a rectangles with height of arr[i] */}
+        </div>
+    )
+}
+
+export default SortingAlgorithm
